beeca66adbc5b6499f8713145ff87bc5da950508
Emelie <empa_petersson@hotmail.com>
2025-04-17 10:47:04 +0200

    Added Radzen

--- FribergHomeClient/Program.cs
+++ FribergHomeClient/Program.cs
@@ -1,11 +1,13 @@
 using FribergHomeClient;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7248") });
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
