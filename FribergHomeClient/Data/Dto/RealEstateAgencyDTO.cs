db815b18106810cbc5560c9c1e010b0887f830bb
Turnquist <tobias.tornquist@yaelev.se>
2025-04-25 15:40:12 +0200

    MyProfile

---
+++ FribergHomeClient/Data/Dto/RealEstateAgencyDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FribergHomeClient.Data.Dto
+{
+    public class RealEstateAgencyDTO
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Presentation { get; set; }
+        public string LogoUrl { get; set; }
+    }
+}
---
+++ FribergHomeClient/Data/Dto/RealEstateAgentDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace FribergHomeClient.Data.Dto
+{
+    public class RealEstateAgentDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ImageUrl { get; set; }
+
+        // Navigation
+        public RealEstateAgencyDTO? Agency { get; set; }
+        public List<PropertyDTO>? Properties { get; set; }
+    }
+}
