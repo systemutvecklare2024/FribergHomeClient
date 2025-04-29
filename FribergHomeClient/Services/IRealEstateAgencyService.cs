ee2c63d8d76037eb33983322f3fe300fa0c2161b
Emelie <empa_petersson@hotmail.com>
2025-04-29 15:40:55 +0200

    Added Service for Agency

---
+++ FribergHomeClient/Services/IRealEstateAgencyService.cs
@@ -0,0 +1,10 @@
+﻿using FribergHomeClient.Data.Dto;
+
+namespace FribergHomeClient.Services
+{
+    public interface IRealEstateAgencyService
+    {
+        Task<List<RealEstateAgencyDTO>> GetAll();
+
+    }
+}
---
+++ FribergHomeClient/Services/RealEstateAgencyService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using FribergHomeClient.Data.Dto;
+using System.Net.Http.Json;
+
+namespace FribergHomeClient.Services
+{
+    public class RealEstateAgencyService : IRealEstateAgencyService
+    {
+        private readonly IMapper mapper;
+        private readonly HttpClient client;
+
+        public RealEstateAgencyService(IMapper mapper, HttpClient client)
+		{
+            this.mapper = mapper;
+            this.client = client;
+        }
+        public async Task<List<RealEstateAgencyDTO>> GetAll()
+        {
+			try
+			{
+                var dtos = await client.GetFromJsonAsync<List<RealEstateAgencyDTO>>($"/api/RealEstateAgencies");
+                return dtos;
+
+            }
+			catch (Exception)
+			{
+
+				throw;
+			}
+        }
+    }
+}
