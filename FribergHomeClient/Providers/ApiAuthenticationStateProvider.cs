88900b31f96dd1aa4dcecba1ba160b076d0e761d
xergon85 <christoffer.isenberg@gmail.com>
2025-04-29 20:03:16 +0200

    my-profile (#15)
    * MyProfile
    
    * MyProfile
    
    * Added Pages for my profile and agent profile
    
    * typo
    
    * AgentId, MyProflie
    
    * Fixed authentication bugs
    
    * wait for agent stuff
    
    * removed unused file
    
    ---------
    
    Co-authored-by: Turnquist <tobias.tornquist@yaelev.se>

--- FribergHomeClient/Providers/ApiAuthenticationStateProvider.cs
+++ FribergHomeClient/Providers/ApiAuthenticationStateProvider.cs
@@ -1,40 +1,48 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Blazored.LocalStorage;
-using FribergHomeClient.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
 
 namespace FribergHomeClient.Providers
 {
 	public class ApiAuthenticationStateProvider : AuthenticationStateProvider
 	{
 		private readonly ILocalStorageService localStorage;
-		private JwtSecurityTokenHandler jwtSecurityTokenHandler;
+        private readonly HttpClient client;
+        private JwtSecurityTokenHandler jwtSecurityTokenHandler;
 
-		public ApiAuthenticationStateProvider(ILocalStorageService localStorage)
+		public ApiAuthenticationStateProvider(ILocalStorageService localStorage, HttpClient client)
 		{
 			this.localStorage = localStorage;
-			jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            this.client = client;
+            jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
 		}
 		public override async Task<AuthenticationState> GetAuthenticationStateAsync()
 		{
 			var user = new ClaimsPrincipal(new ClaimsIdentity());
+
 			var savedToken = await localStorage.GetItemAsync<string>("accessToken");
 			if (savedToken == null)
 			{
+                Console.WriteLine("We have no token hello?");
 				return new AuthenticationState(user);
 			}
 
+            Console.WriteLine($"SavedToken={savedToken}");
+
 			var tokenContent = jwtSecurityTokenHandler.ReadJwtToken(savedToken);
-			if (tokenContent.ValidTo < DateTime.Now)
+			if (tokenContent.ValidTo > DateTime.Now)
 			{
+                Console.WriteLine("Token is old as fook");
 				return new AuthenticationState(user);
 			}
 
 			var claims = GetClaims(tokenContent);
 
 			user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", savedToken);
+            Console.WriteLine("Token is valid, lets authenticate it ffs");
 			return new AuthenticationState(user);
 		}
 
@@ -53,7 +61,7 @@ namespace FribergHomeClient.Providers
 			var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
 
 			var authState = Task.FromResult(new AuthenticationState(user));
-			NotifyAuthenticationStateChanged(authState);
+            NotifyAuthenticationStateChanged(authState);
 		}
 
 		public async Task LoggedOut()
