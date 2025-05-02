c835c95b0bd8a5d622633a9457c692591085e2a0
Turnquist <tobias.tornquist@yaelev.se>
2025-05-02 11:50:14 +0200

    Progress

--- FribergHomeClient/Services/AuthenticationService.cs
+++ FribergHomeClient/Services/AuthenticationService.cs
@@ -39,7 +39,6 @@ namespace FribergHomeClient.Services
 			var token = content.Token;
             var userId = content.UserId;
 			var agentId = content.AgentId;
-
 			await localStorage.SetItemAsync<int>("AgentId", agentId);
 
             await localStorage.SetItemAsync("accessToken", token);
