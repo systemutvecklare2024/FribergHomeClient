ebd4450dd2edea6d03a8b88871e718076ab70d73
Emelie <empa_petersson@hotmail.com>
2025-05-12 16:42:38 +0200

    Added ExtensionMethods

--- FribergHomeClient/Services/RealEstateAgencyService.cs
+++ FribergHomeClient/Services/RealEstateAgencyService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using FribergHomeClient.Data.Dto;
 using FribergHomeClient.Data.ViewModel;
+using FribergHomeClient.Helpers;
 using Radzen;
 using System.Net.Http.Json;
 
@@ -21,135 +22,143 @@ namespace FribergHomeClient.Services
         }
         public async Task<ServiceResponse<List<RealEstateAgencyDTO>>> GetAll()
         {
-			try
-			{
-                var response = await client.GetAsync($"/api/RealEstateAgencies");
-                if (!response.IsSuccessStatusCode)
-                {
-                    var result = new ServiceResponse<List<RealEstateAgencyDTO>>
-                    {
-                        Success = false,
-                        Message = $"Något gick fel: {response.ReasonPhrase}"
-                    };
-                    return result;
-                }
-                return new ServiceResponse<List<RealEstateAgencyDTO>>
-                {
-                    Success = true,
-                    Data = await response.Content.ReadFromJsonAsync<List<RealEstateAgencyDTO>>(),
-                    Message = response.ReasonPhrase ?? ""
-                };
+			//try
+			//{
+   //             var response = await client.GetAsync($"/api/RealEstateAgencies");
+   //             if (!response.IsSuccessStatusCode)
+   //             {
+   //                 var result = new ServiceResponse<List<RealEstateAgencyDTO>>
+   //                 {
+   //                     Success = false,
+   //                     Message = $"Något gick fel: {response.ReasonPhrase}"
+   //                 };
+   //                 return result;
+   //             }
+   //             return new ServiceResponse<List<RealEstateAgencyDTO>>
+   //             {
+   //                 Success = true,
+   //                 Data = await response.Content.ReadFromJsonAsync<List<RealEstateAgencyDTO>>(),
+   //                 Message = response.ReasonPhrase ?? ""
+   //             };
 
-            }
-			catch (HttpRequestException ex)
-			{
+   //         }
+			//catch (HttpRequestException ex)
+			//{
 
-                return new ServiceResponse<List<RealEstateAgencyDTO>>
-                {
-                    Success = false,
-                    Message = $"Något gick fel: {ex.Message}",
-                };
-            }
+   //             return new ServiceResponse<List<RealEstateAgencyDTO>>
+   //             {
+   //                 Success = false,
+   //                 Message = $"Något gick fel: {ex.Message}",
+   //             };
+   //         }
+
+            return await client.GetServiceResponseAsync<List<RealEstateAgencyDTO>>($"/api/RealEstateAgencies");
         }
 
         public async Task<ServiceResponse<RealEstateAgencyPageDTO>> GetById(int id)
         {
-            try
-            {
-                var response = await client.GetAsync($"/api/RealEstateAgencies/{id}");
-                if (!response.IsSuccessStatusCode)
-                {
-                    var result = new ServiceResponse<RealEstateAgencyPageDTO>
-                    {
-                        Success = false,
-                        Message = $"Något gick fel: {response.ReasonPhrase}"
-                    };
-                    return result;
-                }
+            //try
+            //{
+            //    var response = await client.GetAsync($"/api/RealEstateAgencies/{id}");
+            //    if (!response.IsSuccessStatusCode)
+            //    {
+            //        var result = new ServiceResponse<RealEstateAgencyPageDTO>
+            //        {
+            //            Success = false,
+            //            Message = $"Något gick fel: {response.ReasonPhrase}"
+            //        };
+            //        return result;
+            //    }
 
-                return new ServiceResponse<RealEstateAgencyPageDTO>
-                {
-                    Success = true,
-                    Data = await response.Content.ReadFromJsonAsync<RealEstateAgencyPageDTO>(),
-                    Message = response.ReasonPhrase ?? ""
-                };
+            //    return new ServiceResponse<RealEstateAgencyPageDTO>
+            //    {
+            //        Success = true,
+            //        Data = await response.Content.ReadFromJsonAsync<RealEstateAgencyPageDTO>(),
+            //        Message = response.ReasonPhrase ?? ""
+            //    };
 
-            }
-            catch (HttpRequestException ex)
-            {
-                return new ServiceResponse<RealEstateAgencyPageDTO>
-                {
-                    Success = false,
-                    Message = $"Något gick fel: {ex.Message}",
-                };
-            }
+            //}
+            //catch (HttpRequestException ex)
+            //{
+            //    return new ServiceResponse<RealEstateAgencyPageDTO>
+            //    {
+            //        Success = false,
+            //        Message = $"Något gick fel: {ex.Message}",
+            //    };
+            //}
+
+            return await client.GetServiceResponseAsync<RealEstateAgencyPageDTO>($"/api/RealEstateAgencies/{id}");
         }
 
         public async Task<ServiceResponse<RealEstateAgencyPageDTO>> GetMyAgency()
         {
-            try
-            {
-                var response = await client.GetAsync($"api/RealEstateAgencies/My");
-                if (!response.IsSuccessStatusCode)
-                {
-                    var result = new ServiceResponse<RealEstateAgencyPageDTO>
-                    {
-                        Success = false,
-                        Message = $"Något gick fel: {response.ReasonPhrase}"
-                    };
-                    return result;
-                }
+            //try
+            //{
+            //    var response = await client.GetAsync($"api/RealEstateAgencies/My");
+            //    if (!response.IsSuccessStatusCode)
+            //    {
+            //        var result = new ServiceResponse<RealEstateAgencyPageDTO>
+            //        {
+            //            Success = false,
+            //            Message = $"Något gick fel: {response.ReasonPhrase}"
+            //        };
+            //        return result;
+            //    }
 
-                return new ServiceResponse<RealEstateAgencyPageDTO>
-                {
-                    Success = true,
-                    Data = await response.Content.ReadFromJsonAsync<RealEstateAgencyPageDTO>(),
-                    Message = response.ReasonPhrase ?? ""
-                };
+            //    return new ServiceResponse<RealEstateAgencyPageDTO>
+            //    {
+            //        Success = true,
+            //        Data = await response.Content.ReadFromJsonAsync<RealEstateAgencyPageDTO>(),
+            //        Message = response.ReasonPhrase ?? ""
+            //    };
 
-            }
-            catch (HttpRequestException ex)
-            {
-                return new ServiceResponse<RealEstateAgencyPageDTO>
-                {
-                    Success = false,
-                    Message = $"Något gick fel: {ex.Message}",
-                };
-            }
+            //}
+            //catch (HttpRequestException ex)
+            //{
+            //    return new ServiceResponse<RealEstateAgencyPageDTO>
+            //    {
+            //        Success = false,
+            //        Message = $"Något gick fel: {ex.Message}",
+            //    };
+            //}
+            return await client.GetServiceResponseAsync<RealEstateAgencyPageDTO>($"api/RealEstateAgencies/My");
         }
 
-        public async Task<ServiceResponse<bool>> HandleApplication(ApplicationViewModel applicationVM)
+        public async Task<ServiceResponse> HandleApplication(ApplicationViewModel applicationVM)
         {
-            try
-            {
-                var dto = mapper.Map<ApplicationDTO>(applicationVM);
-                var response = await client.PostAsJsonAsync<ApplicationDTO>($"/api/RealEstateAgencies/{dto.AgencyId}/applications/{dto.Id}", dto);
-                if (response.IsSuccessStatusCode)
-                {
-                    var result = new ServiceResponse<bool>()
-                    {
-                        Success = true,
-                        Message = response.ReasonPhrase ?? ""
-                    };
-                    return result;
-                }
+            //try
+            //{
+            //    var dto = mapper.Map<ApplicationDTO>(applicationVM);
+            //    var response = await client.PostAsJsonAsync<ApplicationDTO>($"/api/RealEstateAgencies/{dto.AgencyId}/applications/{dto.Id}", dto);
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        var result = new ServiceResponse<bool>()
+            //        {
+            //            Success = true,
+            //            Message = response.ReasonPhrase ?? ""
+            //        };
+            //        return result;
+            //    }
 
-                return new ServiceResponse<bool>()
-                {
-                    Success = false,
-                    Message = $"Något gick fel: {response.ReasonPhrase}" ?? ""
-                };
+            //    return new ServiceResponse<bool>()
+            //    {
+            //        Success = false,
+            //        Message = $"Något gick fel: {response.ReasonPhrase}" ?? ""
+            //    };
 
-            }
-            catch (HttpRequestException ex)
-            {
+            //}
+            //catch (HttpRequestException ex)
+            //{
 
-                return new ServiceResponse<bool>()
-                {
-                    Success = false,
-                    Message = $"Något gick fel:{ex.Message}" ?? ""
-                };
-            }
+            //    return new ServiceResponse<bool>()
+            //    {
+            //        Success = false,
+            //        Message = $"Något gick fel:{ex.Message}" ?? ""
+            //    };
+            //}
+
+            var dto = mapper.Map<ApplicationDTO>(applicationVM);
+            return await client.PostServiceResponseAsync($"/api/RealEstateAgencies/{dto.AgencyId}/applications/{dto.Id}", dto);
         }
 
         public async Task<List<ApplicationViewModel>> GenerateApplicationViewModels(List<ApplicationDTO> applicationDTOs, List<RealEstateAgentDTO> agentDTOs)
