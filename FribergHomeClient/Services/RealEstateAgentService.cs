9f09dbefc0b2f5bd46390e2c751030076c1f0217
Emelie <empa_petersson@hotmail.com>
2025-05-12 16:44:44 +0200

    Added ExtensionMethods

--- FribergHomeClient/Services/RealEstateAgentService.cs
+++ FribergHomeClient/Services/RealEstateAgentService.cs
@@ -20,137 +20,21 @@ namespace FribergHomeClient.Services
 
         public async Task<ServiceResponse<List<RealEstateAgentDTO>>> GetAll()
         {
-            //try
-            //{
-            //    var response = await client.GetAsync("api/RealEstateAgents");
-            //    if (!response.IsSuccessStatusCode)
-            //    {
-            //        var result = new ServiceResponse<List<RealEstateAgentDTO>>
-            //        {
-            //            Success = false,
-            //            Message = $"Något gick fel: {response.ReasonPhrase}"
-            //        };
-
-            //        return result;
-            //    }
-
-            //    return new ServiceResponse<List<RealEstateAgentDTO>>
-            //    {
-            //        Data = await response.Content.ReadFromJsonAsync<List<RealEstateAgentDTO>>(),
-            //        Success = true,
-            //        Message = response.ReasonPhrase ?? ""
-            //    };
-            //}
-            //catch (HttpRequestException ex)
-            //{
-
-            //    return new ServiceResponse<List<RealEstateAgentDTO>>
-            //    {
-            //        Success = false,
-            //        Message = $"Något gick fel: {ex.Message}",
-            //    };
-            //}
-
             return await client.GetServiceResponseAsync<List<RealEstateAgentDTO>>("api/RealEstateAgents");
         }
         public async Task<ServiceResponse<RealEstateAgentDTO>> GetById(int id)
         {
-            //try
-            //{
-            //    var response = await client.GetAsync($"api/RealEstateAgents/{id}");
-            //    if (!response.IsSuccessStatusCode)
-            //    {
-            //        var result = new ServiceResponse<RealEstateAgentDTO>
-            //        {
-            //            Success = false,
-            //            Message = $"Något gick fel: {response.ReasonPhrase}"
-            //        };
-            //        return result;
-            //    }
-            //    return new ServiceResponse<RealEstateAgentDTO>
-            //    {
-            //        Success = true,
-            //        Message = response.ReasonPhrase ?? "",
-            //        Data = await response.Content.ReadFromJsonAsync<RealEstateAgentDTO>()
-            //    };
-            //}
-            //catch (HttpRequestException ex)
-            //{
-
-            //    return new ServiceResponse<RealEstateAgentDTO>
-            //    {
-            //        Success = false,
-            //        Message = $"Något gick fel: {ex.Message}",
-            //    };
-            //}
-
             return await client.GetServiceResponseAsync<RealEstateAgentDTO>($"api/RealEstateAgents/{id}");
         }
 
         public async Task<ServiceResponse<UpdateAgentDTO>> GetForEditWithMyId()
         {
-            //try
-            //{
-            //    int? id = await GetMyAgentId();
-            //    var response = await client.GetAsync($"api/RealEstateAgents/{id.Value}");
-            //    if (!response.IsSuccessStatusCode)
-            //    {
-            //        var result = new ServiceResponse<UpdateAgentDTO>
-            //        {
-            //            Success = false,
-            //            Message = $"Något gick fel: {response.ReasonPhrase}"
-            //        };
-            //        return result;
-            //    }
-            //    return new ServiceResponse<UpdateAgentDTO>
-            //    {
-            //        Success = true,
-            //        Message = response.ReasonPhrase ?? "",
-            //        Data = await response.Content.ReadFromJsonAsync<UpdateAgentDTO>()
-            //    };
-            //}
-            //catch (HttpRequestException ex)
-            //{
-            //    return new ServiceResponse<UpdateAgentDTO>
-            //    {
-            //        Success = false,
-            //        Message = $"Något gick fel: {ex.Message}",
-            //    };
-            //}
-
             int? id = await GetMyAgentId();
             return await client.GetServiceResponseAsync<UpdateAgentDTO>($"api/RealEstateAgents/{id.Value}");
         }
 
         public async Task<ServiceResponse> UpdateAgentProfile(UpdateAgentDTO agentDTO)
         {
-            //try
-            //{
-            //    var response = await client.PutAsJsonAsync<UpdateAgentDTO>($"api/RealEstateAgents/My", agentDTO);
-            //    if (!response.IsSuccessStatusCode)
-            //    {
-            //        var result = new ServiceResponse
-            //        {
-            //            Success = false,
-            //            Message = $"Något gick fel: {response.ReasonPhrase}"
-            //        };
-            //        return result;
-            //    }
-            //    return new ServiceResponse
-            //    {
-            //        Success = true,
-            //        Message = response.ReasonPhrase ?? "",
-            //    };
-            //}
-            //catch (HttpRequestException ex)
-            //{
-            //    return new ServiceResponse
-            //    {
-            //        Success = false,
-            //        Message = $"Något gick fel: {ex.Message}",
-            //    };
-            //}
-
             return await client.PutServiceResponseAsync("api/RealEstateAgents/My", agentDTO);
         }
 
